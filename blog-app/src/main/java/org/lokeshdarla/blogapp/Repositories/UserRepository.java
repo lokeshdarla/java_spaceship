e4f0ae6a03d81a32b4da5153faba3f738dfc3ad3
lokeshdarla <lokeshnagasaidarla@gmail.com>
2025-01-26 15:57:29 +0530

    [blog-app] feat: protected routes

--- blog-app/src/main/java/org/lokeshdarla/blogapp/Repositories/UserRepository.java
+++ blog-app/src/main/java/org/lokeshdarla/blogapp/Repositories/UserRepository.java
@@ -3,5 +3,8 @@ package org.lokeshdarla.blogapp.Repositories;
 import org.lokeshdarla.blogapp.Models.UserModel;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends MongoRepository<UserModel,String> {
+    Optional<UserModel> findByUsername(String username);
 }
---
+++ blog-app/src/main/java/org/lokeshdarla/blogapp/Security/BlogUserDetailsService.java
@@ -0,0 +1,23 @@
+package org.lokeshdarla.blogapp.Security;
+
+import lombok.RequiredArgsConstructor;
+import org.lokeshdarla.blogapp.Models.UserModel;
+import org.lokeshdarla.blogapp.Repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@RequiredArgsConstructor
+public class BlogUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        UserModel user = this.userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+
+        return new BlogUserDetails(user);
+    }
+}
