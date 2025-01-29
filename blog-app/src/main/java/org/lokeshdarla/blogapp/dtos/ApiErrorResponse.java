d9c502312d219e6eaab9af5cf657851881fdc2ce
lokeshdarla <lokeshnagasaidarla@gmail.com>
2025-01-29 22:32:02 +0530

    exception handling

---
+++ blog-app/src/main/java/org/lokeshdarla/blogapp/dtos/ApiErrorResponse.java
@@ -0,0 +1,27 @@
+package org.lokeshdarla.blogapp.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiErrorResponse {
+    private String message;
+    private int status;
+    private List<FieldError> errors;
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public  static class FieldError{
+        private String field;
+        private String message;
+    }
+}
